2e27038f7b044907f2b227cdd30c230ee801a723
Edvan de Matos <edvan.stt02@gmail.com>
2021-11-05 11:49:01 -0300

    feat: :alien: json-server e integracao com a api adicionados

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,14 +1,28 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import api from "../services/api";
+import { IProduct } from "../store/modules/cart/types";
 
 function Catalog() {
-  const store = useSelector((state) => state);
+  const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-  console.log(store);
+  useEffect(() => {
+    api.get("products").then((response) => setCatalog(response.data));
+  }, []);
 
   return (
-    <div>
+    <main>
       <h1>Catalog</h1>
-    </div>
+
+      {catalog.map((product) => {
+        return (
+          <article key={product.id}>
+            <h3>{product.title}</h3>
+            <span>{product.price} reais</span>{" "}
+            <button type="button">Comprar</button>
+          </article>
+        );
+      })}
+    </main>
   );
 }
 
