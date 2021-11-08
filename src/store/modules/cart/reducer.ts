2bf77780f26434491bf54984ad46faaa896f77f1
Edvan de Matos <edvan.stt02@gmail.com>
2021-11-08 16:28:32 -0300

    feat: :sparkles: reducer integrated with immer immutable lib

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,14 +1,35 @@
 import { Reducer } from "redux";
 import { ICartReducer } from "./types";
+import produce from "immer";
 
 const INITIAL_STATE: ICartReducer = {
   items: [],
 };
 
-const cart: Reducer<ICartReducer> = (state, action) => {
-  switch(action.type) {
-    default: return INITIAL_STATE
-  }
+const cart: Reducer<ICartReducer> = (state = INITIAL_STATE, action) => {
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case "ADD_PRODUCT_TO_CART":
+        const { product } = action.payload;
+
+        const productIndex = draft.items.findIndex(
+          (item) => item.product.id === product.id
+        );
+
+        if (productIndex >= 0) {
+          draft.items[productIndex].quantity++;
+        } else {
+          draft.items.push({
+            product,
+            quantity: 1,
+          });
+        }
+
+        break;
+      default:
+        return draft;
+    }
+  });
 };
 
 export default cart;
